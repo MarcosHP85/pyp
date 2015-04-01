cb9cde9de134370b654520d013010592f3b593e0
Marcos Parodi <marcosparodi85@gmail.com>
2015-04-01 00:40:29 -0300

    DataSources

---
+++ src/main/java/ar/nasa/pyp/config/DataSourcesConfig.java
@@ -0,0 +1,26 @@
+package ar.nasa.pyp.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DataSourcesConfig {
+
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix="spring.datasource.pyp")
+	public DataSource pypDataSource() {
+	    return DataSourceBuilder.create().build();
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix="spring.datasource.ifs")
+	public DataSource ifsDataSource() {
+	    return DataSourceBuilder.create().build();
+	}
+}
