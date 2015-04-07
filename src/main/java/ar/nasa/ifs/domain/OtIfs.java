f03eab0cd7f9a03ff5ce09df3ad1d62983a0e3d7
Marcos Parodi <marcosparodi85@gmail.com>
2015-04-06 23:48:50 -0300

    Ifs casa

--- src/main/java/ar/nasa/ifs/domain/OtIfs.java
+++ src/main/java/ar/nasa/ifs/domain/OtIfs.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "ACTIVE_SEPARATE_OVERVIEW")
+@Table(name = "ACTIVE_SEPARATE_OVERVIEW", schema = "IFSATA")
 public class OtIfs {
 
 	@Id
