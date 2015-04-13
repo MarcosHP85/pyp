b541bc5ef0b01d0718c2ac5821364e579a0bbe44
Marcos Parodi <marcosparodi85@gmail.com>
2015-04-13 02:14:18 -0300

    Remember me

--- src/main/java/ar/nasa/pyp/config/WebSecurityManagerConfig.java
+++ src/main/java/ar/nasa/pyp/config/WebSecurityManagerConfig.java
@@ -1,28 +1,37 @@
 package ar.nasa.pyp.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import ar.nasa.pyp.service.UserServiceImpl;
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class WebSecurityManagerConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	UserServiceImpl userService;
+	@Autowired
+	DataSource dataSource;
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.rememberMe()
-        		.key("kdiaovnfawvioanvavclkcnwq")
+        		.rememberMeServices(rememberMeAuthenticationProvider())
+        		.key("springRocks")
+        		.tokenValiditySeconds(1209600)
         		.and()
             .authorizeRequests()
             	.antMatchers("/resources/**").permitAll()
@@ -37,7 +46,7 @@ public class WebSecurityManagerConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .permitAll();
     }
-	
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) {
 		try {
@@ -45,6 +54,18 @@ public class WebSecurityManagerConfig extends WebSecurityConfigurerAdapter {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Bean
+	public PersistentTokenBasedRememberMeServices rememberMeAuthenticationProvider() {
+		return new PersistentTokenBasedRememberMeServices("springRocks",userService,tokenRepository());
+	}
+	
+	@Bean
+	public PersistentTokenRepository tokenRepository() {
+		JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+		tokenRepository.setDataSource(dataSource);
+		return tokenRepository;
 	}
 	
 	@Bean
