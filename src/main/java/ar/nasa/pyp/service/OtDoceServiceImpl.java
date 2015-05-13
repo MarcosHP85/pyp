ea7c482442e9b58d34e16bfa7df78e80d77b30a7
mparodi <mparodi@XP25.cna1.central.nasa>
2015-05-13 15:00:25 -0300

    DoceService RowToOtDoce

--- src/main/java/ar/nasa/pyp/service/OtDoceServiceImpl.java
+++ src/main/java/ar/nasa/pyp/service/OtDoceServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.format.CellTextFormatter;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -35,8 +36,7 @@ public class OtDoceServiceImpl implements OtDoceService {
 		    Cell cell;
 			
 		    int rowHead = settings.getFirstRow(); // Numero de fila con los titulos
-		    int rows;
-		    rows = sheet.getPhysicalNumberOfRows(); // Total de filas
+		    int rows = sheet.getPhysicalNumberOfRows(); // Total de filas
 
 		    int col = settings.getColNumOt(); // Columna con los numeros de ot
 
@@ -48,35 +48,7 @@ public class OtDoceServiceImpl implements OtDoceService {
 	                		&& row.getCell(settings.getColSemana()).getCellType() == Cell.CELL_TYPE_NUMERIC
 	                		&& (int)row.getCell(settings.getColSemana()).getNumericCellValue() == semana) {
 	                	
-	                	OtDoce ot = new OtDoce();
-	                	
-	                	if(cell.getCellType() == Cell.CELL_TYPE_STRING)
-	                		System.out.print(cell.getStringCellValue()+"\t");
-	                	else if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
-	                		ot.setId((int)cell.getNumericCellValue());
-	                	}
-	                	if(row.getCell(settings.getColSemana()).getCellType() == Cell.CELL_TYPE_NUMERIC) {
-	                		ot.setSemana((int)row.getCell(settings.getColSemana()).getNumericCellValue());
-	                	}
-	                	if(row.getCell(settings.getColObservaciones()).getCellType() == Cell.CELL_TYPE_STRING) {
-	                		ot.setObservaciones(row.getCell(settings.getColObservaciones()).getStringCellValue());
-	                	}
-	                	if(row.getCell(settings.getColLu()).getStringCellValue().equals("X"))
-	                		ot.setLunes(true);
-	                	if(row.getCell(settings.getColMa()).getStringCellValue().equals("X"))
-	                		ot.setMartes(true);
-	                	if(row.getCell(settings.getColMi()).getStringCellValue().equals("X"))
-	                		ot.setMiercoles(true);
-	                	if(row.getCell(settings.getColJu()).getStringCellValue().equals("X"))
-	                		ot.setJueves(true);
-	                	if(row.getCell(settings.getColVi()).getStringCellValue().equals("X"))
-	                		ot.setViernes(true);
-	                	if(row.getCell(settings.getColSa()).getStringCellValue().equals("X"))
-	                		ot.setSabado(true);
-	                	if(row.getCell(settings.getColDo()).getStringCellValue().equals("X"))
-	                		ot.setDomingo(true);
-	                	
-	                	ots.add(ot);
+	                	ots.add(rowToOtDoce(row));
 	                }
 		        }
 		    }
@@ -88,4 +60,33 @@ public class OtDoceServiceImpl implements OtDoceService {
 		return ots;
 	}
 
+	private OtDoce rowToOtDoce(Row row) {
+		OtDoce ot = new OtDoce();
+    	
+    	if(row.getCell(settings.getColNumOt()).getCellType() == Cell.CELL_TYPE_NUMERIC) {
+
+    		ot.setId((int)row.getCell(settings.getColNumOt()).getNumericCellValue());
+
+	    	if(row.getCell(settings.getColSemana()).getCellType() == Cell.CELL_TYPE_NUMERIC)
+	    		ot.setSemana((int)row.getCell(settings.getColSemana()).getNumericCellValue());
+	    	if(row.getCell(settings.getColObservaciones()).getCellType() == Cell.CELL_TYPE_STRING)
+	    		ot.setObservaciones(row.getCell(settings.getColObservaciones()).getStringCellValue());
+	    	if(row.getCell(settings.getColLu()).getStringCellValue().equals("X"))
+	    		ot.setLunes(true);
+	    	if(row.getCell(settings.getColMa()).getStringCellValue().equals("X"))
+	    		ot.setMartes(true);
+	    	if(row.getCell(settings.getColMi()).getStringCellValue().equals("X"))
+	    		ot.setMiercoles(true);
+	    	if(row.getCell(settings.getColJu()).getStringCellValue().equals("X"))
+	    		ot.setJueves(true);
+	    	if(row.getCell(settings.getColVi()).getStringCellValue().equals("X"))
+	    		ot.setViernes(true);
+	    	if(row.getCell(settings.getColSa()).getStringCellValue().equals("X"))
+	    		ot.setSabado(true);
+	    	if(row.getCell(settings.getColDo()).getStringCellValue().equals("X"))
+	    		ot.setDomingo(true);
+    	}
+    	
+    	return ot;
+	}
 }
