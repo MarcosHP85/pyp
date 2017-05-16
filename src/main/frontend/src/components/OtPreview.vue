bb732867a5671db842877f4cf3f08d87ea1076a7
Marcos Parodi <marcosparodi85@gmail.com>
2017-05-16 00:14:04 -0300

    Init OT view

--- src/main/frontend/src/components/OtPreview.vue
+++ src/main/frontend/src/components/OtPreview.vue
@@ -1,14 +1,79 @@
 <template>
   <div style="padding: 16px" v-if="ot !== null && ot !== undefined">
-    <div style="font-size: 20px;padding-bottom: 12px">{{ ot.componente }} / {{ ot.numOt }}</div>
-    <div style="font-size: 16px;text-align: left;padding-bottom: 12px">{{ ot.orgCode }}</div>
-    <div style="font-size: 16px;text-align: left;padding-bottom: 12px">{{ ot.tipoTrabajo }}</div>
-    <div style="font-size: 16px;text-align: left;padding-bottom: 12px">{{ ot.prioridad }}</div>
-    <div style="font-size: 14px;text-align: left;padding-bottom: 12px">{{ fechaRegistro }}</div>
-    <div style="font-size: 14px;text-align: left;padding-bottom: 12px">{{ ot.directiva }}</div>
-    <div style="font-size: 14px;text-align: left;padding-bottom: 12px">{{ ot.tarea }}</div>
-    <div style="font-size: 14px;text-align: left;padding-bottom: 12px">{{ ot.error }}</div>
-    <div style="font-size: 14px;text-align: left;padding-bottom: 12px">{{ ot.comentarioPla }}</div>
+    <el-row>
+      <el-col :span="24">
+        <div id="ot-view-cabecera">
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item>Semana 1720</el-breadcrumb-item>
+            <el-breadcrumb-item>{{ ot.numOt }}</el-breadcrumb-item>
+          </el-breadcrumb>
+          <div class="ot-view-titulo">
+            {{ ot.directiva }}
+          </div>
+          <el-button size="small">
+            <i class="fa fa-comment-o fa-fw"></i> Comentar
+          </el-button>
+          <el-button-group>
+            <el-button size="small">Asignar</el-button>
+            <el-button size="small">Mas</el-button>
+          </el-button-group>
+          <el-button-group style="float: right">
+            <el-button size="small">
+              <i class="fa fa-share-square-o fa-fw"></i> Compartir
+            </el-button>
+            <el-button size="small">
+              <i class="fa fa-download fa-fw"></i>Exportar
+            </el-button>
+          </el-button-group>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="16">
+        <div id="ot-view-main">
+          <div id="ot-view-detalles">
+            <el-row>
+              <el-col :span="4">Tipo:</el-col>
+              <el-col :span="8">{{ ot.tipoTrabajo }}</el-col>
+              <el-col :span="4">Estado:</el-col>
+              <el-col :span="8">{{ ot.estado }}</el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="4">Prioridad:</el-col>
+              <el-col :span="8">{{ ot.prioridad }}</el-col>
+              <el-col :span="4">Org:</el-col>
+              <el-col :span="8">{{ ot.orgCode }}</el-col>
+            </el-row>
+          </div>
+          <div id="ot-view-descripcion">
+            {{ ot.tarea }}
+            {{ ot.error }}
+          </div>
+          <div id="ot-view-relacionadas">
+
+          </div>
+          <div id="ot-view-adjuntos">
+
+          </div>
+          <div id="ot-view-actividades">
+            {{ ot.comentarioPla }}
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div id="ot-view-side">
+          <div id="ot-view-personas">
+
+          </div>
+          <div id="ot-view-fechas">
+            <el-row>
+              <el-col :span="8">Creacion</el-col>
+              <el-col :span="16">{{ fechaRegistro }}</el-col>
+            </el-row>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -28,4 +93,26 @@ export default {
 </script>
 
 <style lang="sass">
+  %ot-view-before
+    white-space: pre
+    font-size: 14px
+    font-weight: 700
+
+  #ot-view-cabecera
+    padding-bottom: 18px
+  #ot-view-detalles::before
+    content: 'Detalles\A'
+    @extend %ot-view-before
+  #ot-view-descripcion::before
+    content: 'Descripcion\A'
+    @extend %ot-view-before
+  #ot-view-actividades::before
+    content: 'Actividades\A'
+    @extend %ot-view-before
+  #ot-view-fechas::before
+    content: 'Fechas\A'
+    @extend %ot-view-before
+  #ot-view-personas::before
+    content: 'Personas\A'
+    @extend %ot-view-before
 </style>
