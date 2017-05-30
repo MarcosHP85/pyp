b147cccb0655b28297c688fe322620b9c6760dfb
Marcos Parodi <marcosparodi85@gmail.com>
2017-05-30 00:25:18 -0300

    Filtro de ots

--- src/main/frontend/src/components/ListaOtsActivas.vue
+++ src/main/frontend/src/components/ListaOtsActivas.vue
@@ -5,12 +5,11 @@
       id="buscar-ot-filtro">
       <el-col
         :span="24">
-        <h2>Filtro</h2>
+        <p class="font-title">Filtrar por &nbsp;</p>
         <el-select
           v-model="filtroComponentes"
           multiple
           filterable
-          allow-create
           placeholder="componente">
           <el-option
             v-for="item in optionComponentes"
@@ -23,7 +22,6 @@
           v-model="filtroPrioridad"
           multiple
           filterable
-          allow-create
           placeholder="prioridad">
           <el-option
             v-for="item in optionPrioridad"
@@ -32,6 +30,23 @@
             :value="item">
           </el-option>
         </el-select>
+        <el-select
+          v-model="filtroOrgMant"
+          multiple
+          filterable
+          placeholder="mantenimiento">
+          <el-option
+            v-for="item in optionOrgMant"
+            :key="item"
+            :label="item"
+            :value="item">
+          </el-option>
+        </el-select>
+        <el-button
+          type="text"
+          @click="reset">
+          Borrar
+        </el-button>
       </el-col>
     </el-row>
     <el-row
@@ -72,10 +87,9 @@ export default {
       listaOts: [],
       otSeleccionada: null,
       cargando: false,
-      // optionComponentes: [],
       filtroComponentes: [],
-      // optionPrioridad: [],
-      filtroPrioridad: []
+      filtroPrioridad: [],
+      filtroOrgMant: []
     }
   },
   mounted () {
@@ -89,7 +103,7 @@ export default {
     optionPrioridad () {
       let tmp = []
       _.forEach(
-        _.groupBy(this.listaOts, 'prioridad'),
+        _.groupBy(this.listaOtsActivas, 'prioridad'),
         (value, key) => {
           tmp.push(key)
         }
@@ -99,7 +113,17 @@ export default {
     optionComponentes () {
       let tmp = []
       _.forEach(
-        _.groupBy(this.listaOts, 'componente'),
+        _.groupBy(this.listaOtsActivas, 'componente'),
+        (value, key) => {
+          tmp.push(key)
+        }
+      )
+      return tmp
+    },
+    optionOrgMant () {
+      let tmp = []
+      _.forEach(
+        _.groupBy(this.listaOtsActivas, 'orgCode'),
         (value, key) => {
           tmp.push(key)
         }
@@ -116,44 +140,55 @@ export default {
       this.cargando = true
       this.buscarOtsActivas(this.ots)
     },
-    // cargarFiltros () {
-    //   _.forEach(
-    //     _.groupBy(this.listaOts, 'componente'),
-    //     (value, key) => {
-    //       this.optionComponentes.push(key)
-    //     }
-    //   )
-      // _.forEach(
-      //   _.groupBy(this.listaOts, 'prioridad'),
-      //   (value, key) => {
-      //     this.optionPrioridad.push(key)
-      //   }
-      // )
-    // },
     mostrarOt (ot) {
       this.otSeleccionada = ot
+    },
+    aplicarFiltros () {
+      let tmp = this.listaOtsActivas
+
+      let componentes = (this.filtroComponentes.length > 0)
+        ? this.filtroComponentes
+        : this.optionComponentes
+      tmp = _.filter(tmp, (ot) => {
+        return componentes.indexOf(ot.componente) !== -1
+      })
+
+      let prioridades = (this.filtroPrioridad.length > 0)
+        ? this.filtroPrioridad
+        : this.optionPrioridad
+      tmp = _.filter(tmp, (ot) => {
+        return prioridades.indexOf(ot.prioridad) !== -1
+      })
+
+      let orgMant = (this.filtroOrgMant.length > 0)
+        ? this.filtroOrgMant
+        : this.optionOrgMant
+      tmp = _.filter(tmp, (ot) => {
+        return orgMant.indexOf(ot.orgCode) !== -1
+      })
+
+      this.listaOts = tmp
+    },
+    reset () {
+      this.filtroComponentes = []
+      this.filtroPrioridad = []
+      this.filtroOrgMant = []
     }
   },
 
   watch: {
     listaOtsActivas: function (val) {
       this.listaOts = val
-      // this.cargarFiltros()
       this.cargando = false
     },
     filtroComponentes: function (val) {
-      this.listaOts = (this.filtroComponentes.length > 0)
-        ? _.filter(this.listaOtsActivas, (ot) => {
-          return this.filtroComponentes.indexOf(ot.componente) !== -1
-        })
-        : this.listaOts
+      this.aplicarFiltros()
     },
     filtroPrioridad: function (val) {
-      this.listaOts = (this.filtroPrioridad.length > 0)
-        ? _.filter(this.listaOtsActivas, (ot) => {
-          return this.filtroPrioridad.indexOf(ot.prioridad) !== -1
-        })
-        : this.listaOts
+      this.aplicarFiltros()
+    },
+    filtroOrgMant: function (val) {
+      this.aplicarFiltros()
     },
     '$route' (to, from) {
       this.cargarLista()
@@ -173,6 +208,15 @@ export default {
   #buscar-ot-filtro
     border-bottom: $el_border
     height: $buscar_ot_filtro_height
+    & > div
+      padding: 8px 40px 8px 24px
+      @include vertical-align-middle
+      & > p
+        display: inline-block
+        padding: 0
+      & > button
+        float: right
+
   #buscar-ot
     height: calc(100% - #{$buscar_ot_filtro_height})
     height: -moz-calc(100% - #{$buscar_ot_filtro_height})
