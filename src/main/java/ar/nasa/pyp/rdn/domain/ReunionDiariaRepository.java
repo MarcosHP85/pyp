83e9894f0a1637015f1bf37995a56cc0d4c885b1
Marcos Parodi <marcosparodi85@gmail.com>
2017-07-05 00:27:25 -0300

    Reunion diraria jpql

--- src/main/java/ar/nasa/pyp/rdn/domain/ReunionDiariaRepository.java
+++ src/main/java/ar/nasa/pyp/rdn/domain/ReunionDiariaRepository.java
@@ -7,7 +7,11 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ReunionDiariaRepository extends CrudRepository<ReunionDiaria, Long> {
 
-	@Query(value = "SELECT top 2 Id,max(Revision) as Revision,IdUnidad,FechaHora FROM Reunion left join Novedades on Reunion.Id = Novedades and Reunion.Revision = Novedades.Revision group by Id,IdUnidad,FechaHora order by FechaHora desc", //SELECT top 2 Id,max(Revision) as Revision,IdUnidad,FechaHora FROM Reunion group by Id,IdUnidad,FechaHora order by FechaHora desc",
-			nativeQuery = true)
+//	@Query(value = "SELECT top 2 Id,max(Revision) as Revision,IdUnidad,FechaHora FROM Reunion left join Novedades on Reunion.Id = Novedades and Reunion.Revision = Novedades.Revision group by Id,IdUnidad,FechaHora order by FechaHora desc", //SELECT top 2 Id,max(Revision) as Revision,IdUnidad,FechaHora FROM Reunion group by Id,IdUnidad,FechaHora order by FechaHora desc",
+//			nativeQuery = true)
+	@Query(value = "select distinct reunion "
+			+ "from ReunionDiaria as reunion "
+			+ "join fetch reunion.novedades "
+			+ "where reunion.fechaHora = (select max(r.fechaHora) from ReunionDiaria r)")
 	List<ReunionDiaria> findUltimasReuniones();
 }
